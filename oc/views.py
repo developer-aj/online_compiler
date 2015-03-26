6660fd97a8218f8d9c3c73ddf11889d8cdbb8ced
Ajay Yadav <ajju.kvmc@gmail.com>
2015-03-27 04:27:58 +0530

    creating views

--- oc/views.py
+++ oc/views.py
@@ -4,16 +4,16 @@ from .forms import PostForm
 
 # Create your views here.
 def post_new(request):
-	if request.method == 'POST':
+	if request.method == "POST":
 		form = PostForm(request.POST)
 		if form.is_valid():
 			post = form.save(commit=False)
 			post.out = "sample output"
 			post.save()
 			return render(request, 'oc/post_detail.html', {'post':post})
-		else:
-			form = PostForm()
-		return render(request, 'oc/post_new.html', {'form':form})
+	else:
+		form = PostForm()
+	return render(request, 'oc/post_new.html', {'form':form})
 
 def post_detail(request, pk):
 	post = get_object_or_404(db, pk=pk)
